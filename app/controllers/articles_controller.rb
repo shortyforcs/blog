1237909b30c50052b6ae7f0ed23e55ef482be130
Adam Holzer <shortyforcs@gmail.com>
2020-08-23 01:33:16 -0700

    Added show and index views and controller definitions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,30 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+  def show
+    @article = Article.find(params[:id])
+  end
   def new
   end
+  def edit
 
+  end
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to @article
+  end
+  def update
+
+  end
+  def destroy
+
+  end
+
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 end
