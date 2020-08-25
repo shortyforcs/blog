3990bf58375843d21777d47d9873ea03b9c6254c
Adam Holzer <shortyforcs@gmail.com>
2020-08-24 18:56:42 -0700

    defined edit/update controller actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,15 +2,19 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
   def show
     @article = Article.find(params[:id])
   end
+
   def new
     @article = Article.new
   end
-  def edit
 
+  def edit
+    @article = Article.find(params[:id])
   end
+
   def create
     @article = Article.new(article_params)
     if @article.save
@@ -18,11 +22,17 @@ class ArticlesController < ApplicationController
     else
       render 'new'
     end
-
   end
+
   def update
-
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
+
   def destroy
 
   end
